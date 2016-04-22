a238cffbd6ba48e735ddee69b6efd7e154a1a0f0
Islam Aliev <islamaliev@Islams-MacBook-Pro.local>
2016-04-22 22:09:19 +0200

    DisplayObject visibility test

--- tests/src/DisplayObjectTest.cpp
+++ tests/src/DisplayObjectTest.cpp
@@ -15,9 +15,20 @@ public:
 TEST_F(DisplayObjectTest, SetWidth) {
     displayObject.setWidth(100);
     ASSERT_EQ(displayObject.width(), 100);
+    displayObject.setWidth(200);
+    ASSERT_EQ(displayObject.width(), 200);
 }
 
 TEST_F(DisplayObjectTest, SetHeight) {
     displayObject.setHeight(100);
     ASSERT_EQ(displayObject.height(), 100);
+    displayObject.setHeight(200);
+    ASSERT_EQ(displayObject.height(), 200);
+}
+
+TEST_F(DisplayObjectTest, setVisible) {
+    displayObject.setVisible(false);
+    ASSERT_EQ(displayObject.visible(), false);
+    displayObject.setVisible(true);
+    ASSERT_EQ(displayObject.visible(), true);
 }

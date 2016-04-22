ce467e8e756e5c0bc22ea9c2c5c69e0ee18b6130
Islam Aliev <islamaliev@Islams-MacBook-Pro.local>
2016-04-22 22:08:53 +0200

    Rectangle constructor test

--- tests/src/RectangleTest.cpp
+++ tests/src/RectangleTest.cpp
@@ -15,24 +15,38 @@ public:
     Rectangle rect;
 };
 
+TEST_F(RectangleTest, Constructor) {
+    Rectangle r = {1, 2, 3, 4};
+    ASSERT_EQ(r.x(), 1);
+    ASSERT_EQ(r.y(), 2);
+    ASSERT_EQ(r.width(), 3);
+    ASSERT_EQ(r.height(), 4);
+}
+
 TEST_F(RectangleTest, SetX) {
     rect.setX(3);
     ASSERT_EQ(rect.x(), 3);
-    rect.setY(5);
-    ASSERT_EQ(rect.y(), 5);
+    rect.setX(7);
+    ASSERT_EQ(rect.x(), 7);
 }
 
 TEST_F(RectangleTest, SetY) {
     rect.setY(5);
     ASSERT_EQ(rect.y(), 5);
+    rect.setY(9);
+    ASSERT_EQ(rect.y(), 9);
 }
 
 TEST_F(RectangleTest, SetWidth) {
     rect.setWidth(5);
     ASSERT_EQ(rect.width(), 5);
+    rect.setWidth(6);
+    ASSERT_EQ(rect.width(), 6);
 }
 
 TEST_F(RectangleTest, SetHeight) {
-    rect.setHeight(5);
-    ASSERT_EQ(rect.height(), 5);
+    rect.setHeight(2);
+    ASSERT_EQ(rect.height(), 2);
+    rect.setHeight(4);
+    ASSERT_EQ(rect.height(), 4);
 }

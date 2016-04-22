bee47f4ae74087cdfeb74493b3270f6db677351e
Islam Aliev <islamaliev@Islams-MacBook-Pro.local>
2016-04-22 21:24:05 +0200

    added tests for width and height

--- tests/src/DisplayObjectTest.cpp
+++ tests/src/DisplayObjectTest.cpp
@@ -1,4 +1,5 @@
 #include <gmock/gmock-matchers.h>
+#include <DisplayObject.h>
 #include "gtest/gtest.h"
 
 using namespace testing;
@@ -7,8 +8,16 @@ class DisplayObjectTest : public testing::Test {
 public:
     void SetUp() override {
     }
+
+    DisplayObject displayObject;
 };
 
-TEST_F(DisplayObjectTest, Bla) {
-    ASSERT_EQ(0, 0);
+TEST_F(DisplayObjectTest, SetWidth) {
+    displayObject.setWidth(100);
+    ASSERT_EQ(displayObject.width(), 100);
+}
+
+TEST_F(DisplayObjectTest, SetHeight) {
+    displayObject.setHeight(100);
+    ASSERT_EQ(displayObject.height(), 100);
 }

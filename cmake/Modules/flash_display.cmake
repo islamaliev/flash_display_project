3bb7b12fd13f887a2551671b28c89feb3682a836
Islam Aliev <islamaliev@Islams-MacBook-Pro.local>
2016-04-23 18:36:20 +0200

    restructured cmake files a bit to improve modularity

---
+++ cmake/Modules/flash_display.cmake
@@ -0,0 +1,8 @@
+set(DISPLAY_LIB flash_display)
+set(${DISPLAY_LIB}_SOURCE_DIR ${CMAKE_SOURCE_DIR}/${DISPLAY_LIB})
+
+add_subdirectory(${${DISPLAY_LIB}_SOURCE_DIR})
+
+set(DISPLAY_LIB_TARGET_NAME ${${DISPLAY_LIB}_TARGET_NAME})
+
+include_directories(${${DISPLAY_LIB}_INCLUDE_DIRS})

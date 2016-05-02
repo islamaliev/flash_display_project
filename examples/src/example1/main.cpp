7bbb89dd9848cb8654606af405de59d5ef3d30a0
Islam Aliev <islamaliev@Islams-MacBook-Pro.local>
2016-05-02 13:29:30 +0200

    example creates 2 display objects, updated pointer to submodule

--- examples/src/example1/main.cpp
+++ examples/src/example1/main.cpp
@@ -2,6 +2,21 @@
 
 int main(int argc, const char** argv) {
     flash::display::Stage stage(800, 600);
+
+    flash::display::DisplayObject obj1;
+    obj1.setWidth(80);
+    obj1.setHeight(20);
+    obj1.setX(20);
+    obj1.setY(20);
+    stage.addChild(&obj1);
+
+    flash::display::DisplayObject obj2;
+    obj2.setWidth(40);
+    obj2.setHeight(100);
+    obj2.setX(740);
+    obj2.setY(480);
+    stage.addChild(&obj2);
+
     stage.start();
 
     return 0;

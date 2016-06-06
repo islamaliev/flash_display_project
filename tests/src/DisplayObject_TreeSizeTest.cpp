939480cb37b67fbbf494ba07d74db9d03db19a42
Islam Aliev <islamaliev@Islams-MacBook-Pro.local>
2016-06-06 22:03:23 +0200

    first tests for treeSize()

---
+++ tests/src/DisplayObject_TreeSizeTest.cpp
@@ -0,0 +1,98 @@
+#include <Stage.h>
+#include "DisplayObjectContainer.h"
+#include "gmock/gmock-matchers.h"
+
+using namespace testing;
+using namespace flash::display;
+
+namespace {
+    const unsigned WIDTH = 40;
+    const unsigned HEIGHT = 20;
+}
+
+class DisplayObject_TreeSizeTest : public Test {
+public:
+    DisplayObject_TreeSizeTest()
+            : stage(WIDTH, HEIGHT) {}
+
+    void makeHierarchy() {
+        stage.addChild(&cont1);
+        cont1.addChild(&cont2);
+        cont2.addChild(&cont3);
+        cont3.addChild(&obj1);
+        cont3.addChild(&obj2);
+        cont3.addChild(&obj3);
+    }
+
+    Stage stage;
+    DisplayObject obj1;
+    DisplayObject obj2;
+    DisplayObject obj3;
+    DisplayObjectContainer cont1;
+    DisplayObjectContainer cont2;
+    DisplayObjectContainer cont3;
+};
+
+TEST_F(DisplayObject_TreeSizeTest, InitiallyIsOne) {
+    ASSERT_THAT(obj1.treeSize(), Eq(1));
+    ASSERT_THAT(cont1.treeSize(), Eq(1));
+    ASSERT_THAT(stage.treeSize(), Eq(1));
+}
+
+TEST_F(DisplayObject_TreeSizeTest, isTwo_ifChildIsAdded) {
+    cont1.addChild(&obj1);
+    ASSERT_THAT(cont1.treeSize(), Eq(2));
+
+    cont2.addChildAt(&cont3, 0);
+    ASSERT_THAT(cont2.treeSize(), Eq(2));
+
+    stage.addChild(&obj2);
+    ASSERT_THAT(stage.treeSize(), Eq(2));
+}
+
+TEST_F(DisplayObject_TreeSizeTest, isOne_ifChildIsRemoved) {
+    cont1.addChild(&obj1);
+    ASSERT_THAT(cont1.treeSize(), Not(1));
+    cont1.removeChild(&obj1);
+    ASSERT_THAT(cont1.treeSize(), Eq(1));
+
+    cont2.addChildAt(&cont3, 0);
+    ASSERT_THAT(cont2.treeSize(), Not(1));
+    cont2.removeChildAt(0);
+    ASSERT_THAT(cont2.treeSize(), Eq(1));
+
+    stage.addChild(&obj2);
+    ASSERT_THAT(stage.treeSize(), Not(1));
+    stage.removeChildren();
+    ASSERT_THAT(stage.treeSize(), Eq(1));
+}
+
+TEST_F(DisplayObject_TreeSizeTest, EveryAddedChildIncreasesTreeSize) {
+    ASSERT_THAT(cont1.treeSize(), Eq(1));
+
+    cont1.addChild(&obj1);
+    ASSERT_THAT(cont1.treeSize(), Eq(2));
+
+    cont1.addChild(&obj2);
+    ASSERT_THAT(cont1.treeSize(), Eq(3));
+
+    cont1.addChild(&cont2);
+    ASSERT_THAT(cont1.treeSize(), Eq(4));
+}
+
+TEST_F(DisplayObject_TreeSizeTest, EveryRemovedChildDecreasesTreeSize) {
+    cont1.addChild(&obj1);
+    cont1.addChild(&obj2);
+    cont1.addChild(&cont2);
+    cont1.addChild(&cont3);
+    ASSERT_THAT(cont1.treeSize(), Eq(5));
+
+    cont1.removeChild(&obj1);
+    ASSERT_THAT(cont1.treeSize(), Eq(4));
+
+    cont1.removeChildAt(0);
+    ASSERT_THAT(cont1.treeSize(), Eq(3));
+
+    cont1.removeChildren();
+    ASSERT_THAT(cont1.treeSize(), Eq(1));
+}

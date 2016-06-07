9466cfc89bc4b78c45c9c4b4198d6688f8d44448
Islam Aliev <islamaliev@Islams-MacBook-Pro.local>
2016-06-07 16:47:12 +0200

    tests for treeSize() for complex hierarchies

--- tests/src/DisplayObject_TreeSizeTest.cpp
+++ tests/src/DisplayObject_TreeSizeTest.cpp
@@ -96,3 +96,120 @@ TEST_F(DisplayObject_TreeSizeTest, EveryRemovedChildDecreasesTreeSize) {
     cont1.removeChildren();
     ASSERT_THAT(cont1.treeSize(), Eq(1));
 }
+
+TEST_F(DisplayObject_TreeSizeTest, EveryAddedDescendantIncreasesTreeSize) {
+    cont1.addChild(&cont2);
+    ASSERT_THAT(cont1.treeSize(), Eq(2));
+
+    cont2.addChild(&cont3);
+    ASSERT_THAT(cont1.treeSize(), Eq(3));
+    ASSERT_THAT(cont2.treeSize(), Eq(2));
+
+    cont3.addChild(&obj1);
+    ASSERT_THAT(cont1.treeSize(), Eq(4));
+    ASSERT_THAT(cont2.treeSize(), Eq(3));
+
+    cont3.addChild(&obj2);
+    ASSERT_THAT(cont1.treeSize(), Eq(5));
+    ASSERT_THAT(cont2.treeSize(), Eq(4));
+}
+
+TEST_F(DisplayObject_TreeSizeTest, EveryRemovedDecsendantDecreasesTreeSize) {
+    makeHierarchy();
+    ASSERT_THAT(stage.treeSize(), Eq(7));
+    ASSERT_THAT(cont1.treeSize(), Eq(6));
+    ASSERT_THAT(cont2.treeSize(), Eq(5));
+
+    cont3.removeChild(&obj1);
+    ASSERT_THAT(stage.treeSize(), Eq(6));
+    ASSERT_THAT(cont1.treeSize(), Eq(5));
+    ASSERT_THAT(cont2.treeSize(), Eq(4));
+
+    cont3.removeChildren();
+    ASSERT_THAT(stage.treeSize(), Eq(4));
+    ASSERT_THAT(cont1.treeSize(), Eq(3));
+    ASSERT_THAT(cont2.treeSize(), Eq(2));
+
+    cont2.removeChildAt(0);
+    ASSERT_THAT(stage.treeSize(), Eq(3));
+    ASSERT_THAT(cont1.treeSize(), Eq(2));
+    ASSERT_THAT(cont2.treeSize(), Eq(1));
+
+    cont1.removeChildren();
+    ASSERT_THAT(stage.treeSize(), Eq(2));
+    ASSERT_THAT(cont1.treeSize(), Eq(1));
+}
+
+TEST_F(DisplayObject_TreeSizeTest, AddingTreeBranch) {
+    stage.addChild(&cont1);
+    cont2.addChild(&obj1);
+    cont3.addChild(&obj2);
+    cont3.addChild(&obj3);
+
+    ASSERT_THAT(cont2.treeSize(), Eq(2));
+    ASSERT_THAT(cont3.treeSize(), Eq(3));
+
+    cont2.addChild(&cont3);
+    ASSERT_THAT(cont2.treeSize(), Eq(5));
+
+    ASSERT_THAT(stage.treeSize(), Eq(2));
+    ASSERT_THAT(cont1.treeSize(), Eq(1));
+
+    cont1.addChildAt(&cont2, 0);
+    ASSERT_THAT(stage.treeSize(), Eq(7));
+    ASSERT_THAT(cont1.treeSize(), Eq(6));
+}
+
+TEST_F(DisplayObject_TreeSizeTest, RemovingTreeBranch) {
+    stage.addChild(&cont1);
+    cont1.addChild(&cont2);
+    cont2.addChild(&obj1);
+    cont2.addChild(&cont3);
+    cont3.addChild(&obj2);
+    cont3.addChild(&obj3);
+
+    ASSERT_THAT(stage.treeSize(), Eq(7));
+    ASSERT_THAT(cont1.treeSize(), Eq(6));
+    ASSERT_THAT(cont3.treeSize(), Eq(3));
+
+    cont2.removeChild(&cont3);
+    ASSERT_THAT(stage.treeSize(), Eq(4));
+    ASSERT_THAT(cont1.treeSize(), Eq(3));
+
+    stage.removeChildAt(0);
+    ASSERT_THAT(stage.treeSize(), Eq(1));
+
+    stage.addChild(&cont1);
+    cont2.addChild(&cont3);
+    ASSERT_THAT(stage.treeSize(), Eq(7));
+    ASSERT_THAT(cont1.treeSize(), Eq(6));
+    ASSERT_THAT(cont2.treeSize(), Eq(5));
+
+    cont2.removeChildren();
+    ASSERT_THAT(stage.treeSize(), Eq(3));
+    ASSERT_THAT(cont1.treeSize(), Eq(2));
+}
+
+TEST_F(DisplayObject_TreeSizeTest, MovingTreeBranch) {
+    stage.addChild(&cont1);
+    stage.addChild(&cont2);
+    cont3.addChild(&obj1);
+    cont3.addChild(&obj2);
+    cont3.addChild(&obj3);
+
+    cont2.addChild(&cont3);
+
+    ASSERT_THAT(stage.treeSize(), Eq(7));
+    ASSERT_THAT(cont1.treeSize(), Eq(1));
+    ASSERT_THAT(cont2.treeSize(), Eq(5));
+
+    cont1.addChild(&cont3);
+    ASSERT_THAT(stage.treeSize(), Eq(7));
+    ASSERT_THAT(cont1.treeSize(), Eq(5));
+    ASSERT_THAT(cont2.treeSize(), Eq(1));
+
+    cont2.addChildAt(&cont3, 0);
+    ASSERT_THAT(stage.treeSize(), Eq(7));
+    ASSERT_THAT(cont1.treeSize(), Eq(1));
+    ASSERT_THAT(cont2.treeSize(), Eq(5));
+}

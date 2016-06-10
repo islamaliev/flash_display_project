3b1d7d6b124b31cdab882fc62c4cea000b696557
Islam Aliev <islamaliev@Islams-MacBook-Pro.local>
2016-06-10 10:25:55 +0200

    tests for proper handling or depth and order components when removing entity

--- tests/src/Component_Test.cpp
+++ tests/src/Component_Test.cpp
@@ -68,32 +68,71 @@ TEST_F(Component_Test, NewEntityDoesNotFail_ifOldOnesAreRemoved) {
 }
 
 TEST_F(Component_Test, RemoveEntityDoesNotAlterOthers) {
-    const auto& i1 = container->createEntity();
-    const auto& i2 = container->createEntity();
-    const auto& i3 = container->createEntity();
-    const auto& i4 = container->createEntity();
-    auto& c1 = container->getSpatialComponent(i1);
-    auto& c2 = container->getSpatialComponent(i2);
-    auto& c3 = container->getSpatialComponent(i3);
-    auto& c4 = container->getSpatialComponent(i4);
+    // create entitis
+    const auto& e1 = container->createEntity();
+    const auto& e2 = container->createEntity();
+    const auto& e3 = container->createEntity();
+    const auto& e4 = container->createEntity();
 
-    c2.width += 2;
-    auto c1Width = c1.width += 1;
-    auto c3Width = c3.width += 3;
-    auto c4Width = c4.width += 4;
+    // get all components for all entities
+    auto& spacial1 = container->getSpatialComponent(e1);
+    auto& spacial2 = container->getSpatialComponent(e2);
+    auto& spacial3 = container->getSpatialComponent(e3);
+    auto& spacial4 = container->getSpatialComponent(e4);
+    auto& depth1 = container->getDepthComponent(e1);
+    auto& depth2 = container->getDepthComponent(e2);
+    auto& depth3 = container->getDepthComponent(e3);
+    auto& depth4 = container->getDepthComponent(e4);
+    auto& order1 = container->getOrderComponent(e1);
+    auto& order2 = container->getOrderComponent(e2);
+    auto& order3 = container->getOrderComponent(e3);
+    auto& order4 = container->getOrderComponent(e4);
 
-    container->removeEntity(i2);
+    // set unique values for components and save value for later comparison
+    spacial2.width += 2;
+    auto width1 = spacial1.width += 1;
+    auto width3 = spacial3.width += 3;
+    auto width4 = spacial4.width += 4;
+    depth2 += 2;
+    auto d1 = depth1 += 1;
+    auto d3 = depth3 += 3;
+    auto d4 = depth4 += 4;
+    order2 += 2;
+    auto o1 = order1 += 1;
+    auto o3 = order3 += 3;
+    auto o4 = order4 += 4;
+
+    // remove entity
+    container->removeEntity(e2);
+
+    // create new one
     const auto& iNew = container->createEntity();
-    auto& cNew = container->getSpatialComponent(iNew);
-    cNew.width += 5;
 
-    c1 = container->getSpatialComponent(i1);
-    c3 = container->getSpatialComponent(i3);
-    c4 = container->getSpatialComponent(i4);
+    // set uniques values for new entity's components
+    container->getSpatialComponent(iNew).width += 5;
+    container->getDepthComponent(iNew) += 5;
+    container->getOrderComponent(iNew) += 5;
 
-    ASSERT_THAT(c1Width, FloatEq(c1.width));
-    ASSERT_THAT(c3Width, FloatEq(c3.width));
-    ASSERT_THAT(c4Width, FloatEq(c4.width));
+    // get components again to compare with
+    spacial1 = container->getSpatialComponent(e1);
+    spacial3 = container->getSpatialComponent(e3);
+    spacial4 = container->getSpatialComponent(e4);
+    depth1 = container->getDepthComponent(e1);
+    depth3 = container->getDepthComponent(e3);
+    depth4 = container->getDepthComponent(e4);
+    order1 = container->getOrderComponent(e1);
+    order3 = container->getOrderComponent(e3);
+    order4 = container->getOrderComponent(e4);
+
+    ASSERT_THAT(width1, FloatEq(spacial1.width));
+    ASSERT_THAT(width3, FloatEq(spacial3.width));
+    ASSERT_THAT(width4, FloatEq(spacial4.width));
+    ASSERT_THAT(depth1, Eq(d1));
+    ASSERT_THAT(depth3, Eq(d3));
+    ASSERT_THAT(depth4, Eq(d4));
+    ASSERT_THAT(order1, Eq(o1));
+    ASSERT_THAT(order3, Eq(o3));
+    ASSERT_THAT(order4, Eq(o4));
 }
 
 TEST_F(Component_Test, UtilizedEntityHasInitialDefaultValues) {
@@ -102,18 +141,32 @@ TEST_F(Component_Test, UtilizedEntityHasInitialDefaultValues) {
     const auto& i3 = container->createEntity();
     const auto& i4 = container->createEntity();
 
-    auto initialValue = container->getSpatialComponent(i1).width;
+    auto initialWidth = container->getSpatialComponent(i1).width;
+    auto initialDepth = container->getDepthComponent(i1);
+    auto initialOrder = container->getOrderComponent(i1);
 
     container->getSpatialComponent(i1).width += 1;
     container->getSpatialComponent(i2).width += 2;
     container->getSpatialComponent(i3).width += 3;
     container->getSpatialComponent(i4).width += 4;
+    container->getDepthComponent(i1) += 1;
+    container->getDepthComponent(i2) += 2;
+    container->getDepthComponent(i3) += 3;
+    container->getDepthComponent(i4) += 4;
+    container->getOrderComponent(i1) += 1;
+    container->getOrderComponent(i2) += 2;
+    container->getOrderComponent(i3) += 3;
+    container->getOrderComponent(i4) += 4;
 
     container->removeEntity(i2);
     const auto& iNew = container->createEntity();
-    auto& cNew = container->getSpatialComponent(iNew);
+    auto newWidth = container->getSpatialComponent(iNew).width;
+    auto newDepth = container->getDepthComponent(iNew);
+    auto newOrder = container->getOrderComponent(iNew);
 
-    ASSERT_THAT(cNew.width, FloatEq(initialValue));
+    ASSERT_THAT(newWidth, FloatEq(initialWidth));
+    ASSERT_THAT(newDepth, Eq(initialDepth));
+    ASSERT_THAT(newOrder, Eq(initialOrder));
 }
 
 TEST_F(Component_Test, ForEach) {
